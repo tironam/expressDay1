ddf5faa97c628d2869fa5a204bb6e209a25e6e1a
Michael Tirona <tironamichael@gmail.com>
2020-05-18 11:42:11 -0700

    Got both files working

--- calc.js
+++ calc.js
@@ -1,7 +1,29 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
+    // splits the url up into different pieces and this will show you how to pick the pieces
+    console.log(req.url.split('/'))
+    const pieces = req.url.split('/')
+    // Don't gorget to parse a and b as integers or they'll be strings you can't add together
+    const op = pieces[1]
+    const a = parseInt(pieces[2])
+    const b = parseInt(pieces[3])
 
+    // You have to use temperate literals because it can't pass a number, only a string
+    switch (op) {
+        case 'add':
+            res.end(`${a + b}`)
+            break
+        case 'subtract':
+            res.end (`${a - b}`)
+            break
+        case 'multiply':
+            res.end(`${a * b}`)
+            break
+        case 'divide':
+            res.end(`${a / b}`)
+            break
+    }
 })
 
 server.listen(3000, () => {
--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/movies') {
-        res.end(
-            'Scott Pilgrim, Ghostbusters, Back to the Future, Hot Fuzz, La La Land'
-            )
-    } else if 
-        (req.url === '/games') {
-            res.end('Breath of the Wild, Portal 2, Tony Hawk\'s Pro Skater, Rock Band, Spider-Man')
-        } else if (req.url === '/songs') {
-            res.end('Hands Down by Dashboard Confessiona, Jack Sparrow by The Lonely Island, Perfect Situation by Weezer, Sunflower by Post Malone, Grand Theft Autumn by Fall Out Boy')
-        } else if (req.url === '/foods') {
-            res.end('Pizza, Burgers, Steak, Breakfast Burritos, Fried Chicken')
-        }
+    // This was my code, but you want to use switch cases
+
+    // if (req.url === '/movies') {
+    //     res.end(
+    //         'Scott Pilgrim, Ghostbusters, Back to the Future, Hot Fuzz, La La Land'
+    //         )
+    // } else if 
+    //     (req.url === '/games') {
+    //         res.end('Breath of the Wild, Portal 2, Tony Hawk\'s Pro Skater, Rock Band, Spider-Man')
+    //     } else if (req.url === '/songs') {
+    //         res.end('Hands Down by Dashboard Confessiona, Jack Sparrow by The Lonely Island, Perfect Situation by Weezer, Sunflower by Post Malone, Grand Theft Autumn by Fall Out Boy')
+    //     } else if (req.url === '/foods') {
+    //         res.end('Pizza, Burgers, Steak, Breakfast Burritos, Fried Chicken')
+    //     }
+
+    switch (rteq.url) {
+        case '/movies':
+            break
+        case '/games':
+            break
+        case '/songs':
+            break
+        case '/foods':
+            break
+    }
 })
 
 server.listen(3000, () => {
